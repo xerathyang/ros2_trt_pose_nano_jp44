6a2cb423000c1804140a0af53d61e67ea0138281
TzuShianYang <c4940578@gmail.com>
2022-09-04 20:25:48 +0800

    inital add

---
+++ docker_run_new.sh
@@ -0,0 +1,2 @@
+sudo xhost +local:docker
+sudo docker run -it --rm --privileged --runtime nvidia -e DISPLAY=$DISPLAY --device="/dev/video0:/dev/video0" --network host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /home/ak-nv/ros_docker:/workdir ros2_trt_pose_base:jp44
