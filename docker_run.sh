e15bf5c38159165bea9aff4689088c673a35739a
TzuShianYang <c4940578@gmail.com>
2022-09-12 22:40:31 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 sudo xhost +
-sudo docker run -it --rm --privileged --runtime nvidia -e DISPLAY=$DISPLAY --device="/dev/video0:/dev/video0" --ipc=host --network host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /home/ak-nv/ros_docker:/workdir ros2_trt_pose_base:jp44
+sudo docker run -it --rm --privileged --runtime nvidia -e DISPLAY=$DISPLAY --device="/dev/video0:/dev/video0" --ipc=host --network host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /home/ak-nv/ros_docker:/workdir ros2_trt_pose_nano:jp44
